9545407ada783f6545716dbabbae85f7359f566f
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2020-03-16 19:03:35 -0400

    Update OpenRewriteBetaTest.lua

--- OpenRewriteBetaTest.lua
+++ OpenRewriteBetaTest.lua
@@ -668,7 +668,7 @@ local WChild = workspace:GetChildren()
 	for i = 1,#WChild do 
 		if WChild[i].Name == "Door" and WChild[i]:FindFirstChild'Click' and WChild[i]:FindFirstChild'Lock' then
 			WChild[i].Lock.ClickDetector:FindFirstChildOfClass'RemoteEvent':FireServer()
-			WChild[i].Click.ClickDetector:FindFirstChildOfClass'RemoteEvent':FireServer(
+			WChild[i].Click.ClickDetector:FindFirstChildOfClass'RemoteEvent':FireServer()
 			wait()
 		end
 	end
