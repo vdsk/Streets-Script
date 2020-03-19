f3d5cc958b08e6b81fb52d03f534c08fc3fc3e5e
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2020-03-19 03:21:54 -0400

    Update Streets Admin.lua

--- Streets Admin.lua
+++ Streets Admin.lua
@@ -1209,7 +1209,7 @@ AddCommand(function(Arguments)
 			end
 		else 
 			local Player = PlrFinder(Arguments[1])
-			if not Player or Playerthen return end 
+			if not Player or Player then return end 
 			if Player ~= LP then
 				table.insert(EspTable,Player.UserId)
 				if not UseDraw or Arguments[2] and Arguments[2]:lower() == "legacy" then 
