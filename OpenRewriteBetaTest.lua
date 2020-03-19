d93c3706a04e6b4d30757bf5e1127a7ddf15e794
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2020-03-19 02:28:54 -0400

    Delete OpenRewriteBetaTest.lua

--- OpenRewriteBetaTest.lua
+++
@@ -1,1 +0,0 @@
-game:GetService'StarterGui':SetCore("SendNotification",{['Title'] = "Use the old loadstring!";['Text'] = "if you don't know what the loadstring is join https://discord.gg/UVgdNXP";['Duration'] = 100;})
