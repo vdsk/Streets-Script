44d37bc3bc8dc32fc20b510736c03c0b8bbf489f
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2020-04-23 04:20:09 -0400

    Update Streets Admin.lua

--- Streets Admin.lua
+++ Streets Admin.lua
@@ -1604,7 +1604,7 @@ local WhitelistedOs = {
 	['ios'] = "Apple";
 	['android'] = "Superior Android";
 	['osx'] = "Mac (GROSS)";
-	['Windows_Universal'] = "Windows 10 roblox"
+	['windows_universal'] = "Windows 10 roblox"
 }
 
 local function Stepped()
