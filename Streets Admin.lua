2ecb57e81529be840890606f4142773a42f99466
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2020-07-03 19:27:40 -0400

    blacklisted "matt" the carpet

--- Streets Admin.lua
+++ Streets Admin.lua
@@ -278,6 +278,7 @@ local BlacklistTable = {
 	[878779033] = true; -- retard who hit me
 	[1052869632] = true; -- Generally annoying and fan boys tagged users and claims he can script when he can't (skid).
 	[481265818] = true; -- said he would dox me but wouldn't
+	[872132185] = true; -- who names their child "matt" 
 }
 
 local SettingsTable = {
